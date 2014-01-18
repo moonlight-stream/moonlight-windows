cccb214e07344df3a33ff360421e2893b27b3a4c
Michelle Bergeron <mrb113@case.edu>
2014-01-18 04:17:28 -0500

    Add UDP socket skeleton

---
+++ Limelight/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Limelight.Resources;
+
+namespace Limelight
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
