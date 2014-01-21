4c04aa56cb5bf3f0d5e94272041cbddd7f1eb9c7
Michelle Bergeron <mrb113@case.edu>
2014-01-21 15:45:36 -0500

    Add video decoder class skeleton

--- Limelight/StreamFrame.xaml.cs
+++ Limelight/StreamFrame.xaml.cs
@@ -12,11 +12,12 @@ namespace Limelight
         public VideoStreamSource videoStream;
 
         /// <summary>
-        /// Begins stream to the MediaElement
+        /// Begins stream to the MediaElement on the Application Frame
         /// </summary>
         public StreamFrame()
         {
             InitializeComponent();
+
             videoStream = new VideoStreamSource(null, frameWidth, frameHeight);
             StreamDisplay.SetSource(videoStream);  
             StreamDisplay.AutoPlay = true;
@@ -24,10 +25,26 @@ namespace Limelight
 
             // Starts the demo video renderer in a new thread
             ThreadPool.QueueUserWorkItem(hacks);
+
+            // TODO use Limelight Common to get the stream
+
+            // TODO uncomment when you have a real stream to use
+            //ThreadPool.QueueUserWorkItem(Decoder);
+
         }
         public void hacks(object o)
         {
             new H264FileReaderHackery(this).readFile();
+        }
+
+        /// <summary>
+        /// Begins the decoder
+        /// </summary>
+        /// <param name="o"></param>
+        public void Decoder(object o)
+        {
+            // TODO pass in resource stream
+            new VideoDecoder(this).decodeVideo(null); 
         }
     }
 }
---
+++ Limelight/VideoDecoder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Resources;
+using Windows.Storage.Streams;
+
+namespace Limelight
+{
+    class VideoDecoder
+    {
+        private StreamFrame frame;
+        public VideoDecoder(StreamFrame frame)
+        {
+            this.frame = frame;
+        }
+
+        /// <summary>
+        /// Decodes the H264 video stream for rendering
+        /// </summary>
+        /// <param name="resourceStream">The resource stream for the video</param>
+        public void decodeVideo(StreamResourceInfo resourceStream)
+        {
+            Debug.WriteLine("[VideoDecoder] Decoding video...");
+
+            Stream stream = resourceStream.Stream;
+            IBuffer mediaStream;
+            int seekOffset = 0;
+
+            // 128 kilobyte buffer
+            Byte[] buffer = new Byte[131072];
+
+            while (stream.CanRead)
+            {
+                int len = stream.Read(buffer, 0, buffer.Length);
+                if (len > 0)
+                {
+                    bool firstStart = false;
+                    for (int i = 0; i < len - 4; i++)
+                    {
+                        seekOffset++;
+                        if (buffer[i] == 0 && buffer[i + 1] == 0 && buffer[i + 2] == 0 && buffer[i + 3] == 1)
+                        {
+                            if (firstStart)
+                            {
+                                // Put the buffer contents into a format that the video stream source can use
+                                mediaStream = buffer.AsBuffer();
+
+                                frame.videoStream.TransportController_VideoMessageReceived(mediaStream, 0, 10);
+                                stream.Position = --seekOffset;
+                            }
+                            else
+                            {
+                                firstStart = true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine("[VideoDecoder] No buffer");
+
+                    // TODO When reading the file, resetting these make it loop the video
+                    // The decoder isn't for reading from a file
+                    stream.Position = 0;
+                    seekOffset = 0;
+                }
+            }
+        }
+    }
+}
