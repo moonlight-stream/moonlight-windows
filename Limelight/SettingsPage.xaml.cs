046e83747531ac6839ed79a8cd056bc9877ee608
Dave Jannotta <daj40@case.edu>
2014-04-24 01:47:05 -0400

    Made more UX changes, including making the keyboard more helpful (not as helpful as I wanted it to be, but its better than the normal one), made buttons prettier and ghosted the text in the input box.

--- Limelight/SettingsPage.xaml.cs
+++ Limelight/SettingsPage.xaml.cs
@@ -10,11 +10,69 @@ using Microsoft.Phone.Shell;
 
 namespace Limelight
 {
+    public enum Resolutions { WVGA, WXGA, HD };
     public partial class SettingsPage : PhoneApplicationPage
     {
         public SettingsPage()
         {
             InitializeComponent();
+
+
+            //for when we dynamically tell what resolution the phone is
+            /*
+            List<String> ResolutionAndFrameRateSource = new List<String>();
+            switch(ResolutionHelper.CurrentResolution)
+            {
+                case Resolutions.HD:
+                    ResolutionAndFrameRateSource.Add("720p (30FPS)");
+                    ResolutionAndFrameRateSource.Add("720p (60FPS)");
+                    break;
+                default:
+                    ResolutionAndFrameRateSource.Add("YOU BROKE SOMETHING");
+                    ResolutionAndFrameRateSource.Add("Or no HD modes are avaialble.");
+                    break;
+            }
+            */
+        }
+    }
+
+    
+
+    public static class ResolutionHelper
+    {
+        private static bool IsWvga
+        {
+            get
+            {
+                return App.Current.Host.Content.ScaleFactor == 100;
+            }
+        }
+
+        private static bool IsWxga
+        {
+            get
+            {
+                return App.Current.Host.Content.ScaleFactor == 160;
+            }
+        }
+
+        private static bool IsHD
+        {
+            get
+            {
+                return App.Current.Host.Content.ScaleFactor == 150;
+            }
+        }
+
+        public static Resolutions CurrentResolution
+        {
+            get
+            {
+                if (IsWvga) return Resolutions.WVGA;
+                else if (IsWxga) return Resolutions.WXGA;
+                else if (IsHD) return Resolutions.HD;
+                else throw new InvalidOperationException("Unknown resolution");
+            }
         }
     }
 }
