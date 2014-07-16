e56aa383844c643391bf10e30af6e97cbbcef6c9
Michelle Bergeron <mrb113@case.edu>
2014-07-16 13:48:56 -0500

    Modify classes to build for Windows Runtime 8.1

---
+++ Limelight_new/NvHttp.cs
@@ -0,0 +1,76 @@
+﻿using Windows.Networking;
+using Windows.Networking.Sockets; 
+using System;
+using System.Net;
+using System.Threading;
+using Windows.System.Profile;
+using Windows.Storage.Streams;
+
+namespace Limelight_new
+{
+    /// <summary>
+    /// Object containing the hostname and methods to resolve it
+    /// </summary>
+    public class NvHttp 
+    {
+	    public const int PORT = 47989;
+	    public const int CONNECTION_TIMEOUT = 5000;
+        public String baseUrl { get; set; }
+        public HostName resolvedHost {get; set;}
+
+        #region Public Methods
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NvHttp"/> class. 
+        /// </summary>
+        /// <param name="hostnameString">Hostname or IP address of the streaming machine</param>
+        public NvHttp(String hostnameString)
+        {
+            if (hostnameString == null)
+            {
+                throw new ArgumentNullException("Hostname cannot be null");
+            }
+            completeEvent = new ManualResetEvent(false);
+            ResolveHostName(hostnameString);
+            this.baseUrl = "http://" + resolvedHost.ToString() + ":" + PORT;
+        }
+
+        /// <summary>
+        /// Get the local device's name
+        /// </summary>
+        /// <returns>Unique ID for the device</returns>
+        public String GetDeviceName()
+        {
+            var token = HardwareIdentification.GetPackageSpecificToken(null);
+            var hardwareId = token.Id;
+            var dataReader = Windows.Storage.Streams.DataReader.FromBuffer(hardwareId);
+
+            byte[] bytes = new byte[hardwareId.Length];
+            dataReader.ReadBytes(bytes);
+
+            return BitConverter.ToString(bytes); 
+        }
+        #endregion Public Methods
+
+        #region Private Methods
+
+        /// <summary>
+        /// Resolve the GEForce PC hostname to an IP Address
+        /// </summary>
+        /// <param name="hostName"></param>
+        private async void ResolveHostName(String hostName)
+        {
+            HostName serverHost = new HostName("www.google.com");
+            StreamSocket clientSocket = new Windows.Networking.Sockets.StreamSocket();
+
+            // Try to connect to the remote host
+            await clientSocket.ConnectAsync(serverHost, "http");
+
+            // Now try the clientSocket.Information property
+            // e.g. clientSocket.Information.RemoteAddress
+            // to get the ip address
+            this.resolvedHost = clientSocket.Information.RemoteAddress;
+        }
+        #endregion Private Methods
+
+    }
+}
---
+++ Limelight_new/XmlQuery.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+namespace Limelight_new
+{
+    /// <summary>
+    /// XmlQuery object contains an XML string and methods to parse it
+    /// </summary>
+    public class XmlQuery
+    {
+        private Uri uri;
+        private XDocument rawXml;
+        private string rawXmlString;
+        private string err = null;
+
+        #region Public Methods
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="XmlQuery"/> class. 
+        /// </summary>
+        /// <param name="url">URL of XML page</param>
+        public XmlQuery(string url)
+        {
+            uri = new Uri(url);
+            GetXml(); 
+        }
+
+        /// <summary>
+        /// Given a tag, return the first XML attribute contained in this tag as a string
+        /// </summary>
+        /// <param name="tag">Tag containing the desired attribute</param>
+        /// <returns>The first attribute within the given tag</returns>
+        public string XmlAttribute(string tag)
+        {
+            // TODO handle not found
+            var query = from c in rawXml.Descendants(tag) select c;
+            string attribute = query.FirstOrDefault().Value;
+            return attribute;
+        }
+
+        /// <summary>
+        /// Given an XElement and a tag, search within that element for the first attribute contained within the tag
+        /// </summary>
+        /// <param name="tag">XML tag</param>
+        /// <param name="element">XElement to search within</param>
+        /// <returns>The first attribute within the given tag in the XElement</returns>
+        public string XmlAttribute(string tag, XElement element)
+        {
+            // TODO handle not found
+            var query = from c in element.Descendants(tag) select c;
+            string attribute = query.FirstOrDefault().Value;
+            return attribute;
+        }
+
+        /// <summary>
+        /// Given a tag, return the first XML attribute contained in this tag as an XElement
+        /// </summary>
+        /// <param name="tag">Tag containing the desired attribute</param>
+        /// <returns>The first attribute within the given tag</returns>
+        public XElement XmlAttributeElement(string tag)
+        {
+            // TODO handle not found
+            var query = from c in rawXml.Descendants(tag) select c;
+            return query.FirstOrDefault(); 
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+        /// <summary>
+        /// Gets the Xml as a string from the URL provided
+        /// </summary>
+        /// <returns>The server info XML as a string</returns>
+        private async Task GetXml()
+        {
+            Debug.WriteLine(uri);
+            if (rawXmlString == null)
+            {
+                HttpClient client = new HttpClient();
+                // Throws HttpClientException if something goes wrong
+                rawXmlString = await client.GetStringAsync(uri);
+                this.rawXml = XDocument.Parse(rawXmlString);
+
+            }
+        }
+        #endregion Private Methods
+
+    }
+}
